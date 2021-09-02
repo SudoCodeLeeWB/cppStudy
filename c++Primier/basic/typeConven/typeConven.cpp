e9c59e98e78a362a5de6a9920cc882d1170c8f27
SudoCodeLeeWB <84519146+SudoCodeLeeWB@users.noreply.github.com>
2021-09-02 19:23:19 +0800

    commit

--- c++Primier/basic/typeConven/typeConven.cpp
+++ c++Primier/basic/typeConven/typeConven.cpp
@@ -18,15 +18,37 @@ int main(){
     double pi =i;
     cout << pi <<endl;   
 
-//two variables : c1 and c2 generates error. 
+    //two variables : c1 and c2 generates error. 
 
     //unsignd char is 0 to 255
+    //expressions lnvloving the unsigned types.
     unsigned char c1 = -1;
     cout << c1 <<endl;   
+
+    //unsigned also effectes the loop. 
+    for( unsigned int i =0; i > -10 ; i--){
+
+
+
+    }
     
     // signed char is -128 to 127
     signed char c2 = 256;
     cout << c2 <<endl;   
+
+
+
+    int i =42;
+    // if the value of the i is 0 then the condition is false, else , the condition is true. 
+    if(i) {
+    i =0;
+    cout << i << endl;
+
+    }
+
+
+   
+
 
 
     return 0;
